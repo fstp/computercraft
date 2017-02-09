d0ed988c0664a8d187bd59e6c27777a368aafe24
John Blomberg <johnblo@kth.se>
2017-02-09 13:38:37 +0100

    Group rows and transform groups of one element into lua.
    Added some support functions to keep the total lines of code down in large
    programs. The idea is to group each row into consecutive runs of the same action
    and then compress it by emitting just a for-loop instead of repeating the
    actions over and over again.

--- lib/gen.ex
+++ lib/gen.ex
@@ -3,7 +3,6 @@ defmodule Gen do
     File.read!("blueprint.txt")
     |> String.split("\n", trim: true)
     |> create_layers(%{layers: []})
-    |> trace
   end
 
   def create_layers([head | tail], state) do
@@ -22,8 +21,61 @@ defmodule Gen do
     |> Enum.map(&parse_row/1)
   end
 
-  def parse_row(row) do
-    row
+  def parse_row({data, idx}) do
+    data
+    |> String.split("", trim: true)
+    |> group
+    |> Enum.map(fn group -> to_lua(group, length(group)) end)
+    |> trace
+  end
+
+  def to_lua(["0"], 1) do
+    ["dig()\n"]
+  end
+  def to_lua([slot], 1) when is_binary(slot) do
+    ["place(", slot, ")\n"]
+  end
+  def to_lua(slots, length) do
+    slots
+  end
+
+  def lua_fun_turn_around() do
+    ["function turnAround()\n",
+     "turtle.turnLeft()\n",
+     "turtle.turnLeft()\n",
+     "end\n"]
+  end
+  def lua_fun_place() do
+    ["function place(slot)\n",
+     "turtle.dig()\n",
+     "turtle.forward()\n",
+     "turnAround()\n",
+     "turtle.select(slot)\n",
+     "turtle.place()\n",
+     "turnAround()\n",
+     "end\n"]
+  end
+  def lua_fun_dig() do
+    ["function dig()\n",
+     "turtle.dig()\n",
+     "turtle.forward()\n",
+     "end\n"]
+  end
+
+  def group(items) do
+    group(items, [[]])
+  end
+  def group([head | tail], [[head | _] = acc_head | acc_tail]) do
+    group(tail, [[head | acc_head] | acc_tail])
+  end
+  def group([head | tail], [[]]) do
+    group(tail, [[head]])
+  end
+  def group([head | tail], acc) do
+    group(tail, [[head] | acc])
+  end
+  def group([], acc) do
+    Enum.reverse(acc)
   end
 
   def parse_dimensions(line) do
